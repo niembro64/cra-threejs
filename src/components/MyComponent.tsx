1d819b3a4212db4327dccfa4e3fbeb0232bac43e
Eric Niemeyer <niemeyer.eric@gmail.com>
2023-03-13 00:27:29 -0400

    donut

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -18,20 +18,32 @@ const MyThree: React.FC<MyThreeProps> = () => {
       1000
     );
     var renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     // document.body.appendChild( renderer.domElement );
     // use ref as a mount point of the Three.js scene instead of the document.body
+    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+    const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+    const torus = new THREE.Mesh(geometry, material);
+
+    scene.add(torus);
+
+    camera.position.z = 30;
+
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(50, 10, 15);
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    ambientLight.intensity = 0.01;
+    scene.add(pointLight);
+    scene.add(ambientLight);
+
     refContainer.current &&
       refContainer.current.appendChild(renderer.domElement);
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube: any = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
     var animate = function () {
       requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
+      torus.rotation.x += 0.003;
+      torus.rotation.y += 0.001;
+      torus.rotation.z += 0.002;
       renderer.render(scene, camera);
     };
     animate();
