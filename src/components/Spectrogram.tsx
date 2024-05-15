3b43ab0f7f37adffa59030889cfd7e16d645c7ce
Eric Niemeyer <niemeyer.eric@gmail.com>
2024-05-15 00:05:57 -0400

    mel spectrum

--- src/components/Spectrogram.tsx
+++ src/components/Spectrogram.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef } from 'react';
 import Meyda, { MeydaFeaturesObject } from 'meyda';
+import { MeydaAnalyzer } from 'meyda/dist/esm/meyda-wa';
 
 const AudioSpectrogram: React.FC = () => {
   const audioContextRef = useRef<AudioContext | null>(null);
   const microphoneRef = useRef<MediaStreamAudioSourceNode | null>(null);
-  const meydaAnalyzerRef = useRef<Meyda.MeydaAnalyzer | null>(null);
+  const meydaAnalyzerRef = useRef<MeydaAnalyzer | null>(null);
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
@@ -12,28 +13,37 @@ const AudioSpectrogram: React.FC = () => {
       try {
         const audioContext = new AudioContext();
         audioContextRef.current = audioContext;
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
         const source = audioContext.createMediaStreamSource(stream);
         microphoneRef.current = source;
 
         meydaAnalyzerRef.current = Meyda.createMeydaAnalyzer({
           audioContext: audioContext,
           source: source,
-          bufferSize: 512,
-          featureExtractors: ['mfcc'],
+          bufferSize: 512 * 2,
+          featureExtractors: ['melBands'],
+          melBands: 16, // Number of mel bands
           callback: (features: MeydaFeaturesObject) => {
-            if (features && features.mfcc && canvasRef.current) {
+            // @ts-ignore
+            if (features && features.melBands && canvasRef.current) {
               const canvas = canvasRef.current;
               const ctx = canvas.getContext('2d');
               if (ctx) {
                 ctx.fillStyle = 'black';
                 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-                features.mfcc.forEach((mfccValue, index) => {
-                  const normalizedValue = (mfccValue + 40) / 80; // Normalization step
+                // @ts-ignore
+                features.melBands.forEach((melValue, index) => {
+                  const normalizedValue = melValue / 10; // Normalize based on expected max value
                   const height = normalizedValue * canvas.height;
-                  const width = canvas.width / features.mfcc.length;
-                  ctx.fillStyle = `hsl(${index * 10}, 100%, 50%)`;
+                  // @ts-ignore
+                  const width = canvas.width / features.melBands.length;
+                  ctx.fillStyle = `hsl(${
+                    // @ts-ignore
+                    360 * (index / features.melBands.length)
+                  }, 100%, 50%)`;
                   ctx.fillRect(
                     index * width,
                     canvas.height - height,
@@ -64,11 +74,13 @@ const AudioSpectrogram: React.FC = () => {
   return (
     <div>
       <h1>Mel-Frequency Spectrogram</h1>
+      {/* @ts-ignore */}
       <canvas
+        className="canvas-spectrogram"
         ref={canvasRef}
         width="1024"
-        height="400"
-        style={{ background: 'black' }}
+        height="100"
+        // style={{ background: 'black' }}
       ></canvas>
     </div>
   );
