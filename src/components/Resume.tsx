226fc4615299ffd49d6da933b8ed3a471fc614b5
Eric Niemeyer <niemeyer.eric@gmail.com>
2026-08-12 01:21:10 -0400

    ok

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -22,15 +22,11 @@ export const Resume: React.FC<ResumeProps> = () => {
     <div className="main-top w-full py-4 md:px-12">
       {isThin && (
         <>
-          <div className="h-40" />
-          <div className="h-40" />
-          <div className="h-40" />
-          <PixelArtText scrollContainerSelector=".pixel-text-niemo-io" pixelColor="#fff" text=" NIEMO.IO " />
+          <section className="px-4 py-24 text-center">
+            <PixelArtText scrollContainerSelector=".pixel-text-niemo-io" pixelColor="#fff" text=" NIEMO.IO " />
+          </section>
 
-          <div className="h-40" />
-          <div className="h-40" />
-          <div className="h-40" />
-          <section className={`px-4 py-12 ${isThin ? 'bg-black/80' : ''} text-center`}>
+          <section className={`px-4 py-24 ${isThin ? 'bg-black/80' : ''} text-center`}>
             {showProfileAbout && (
               <div className="m-8 flex flex-row items-center justify-center overflow-hidden rounded-2xl">
                 <img
@@ -48,7 +44,6 @@ export const Resume: React.FC<ResumeProps> = () => {
               pixelColor="#fff"
               text=" ERIC NIEMEYER "
             />
-            <p className="pixel-font mt-4 text-2xl uppercase text-blue-300">Stamford, Connecticut</p>
 
             <div className="mb-4 flex flex-row items-center justify-center">
               {showSmashedGif ? (
@@ -59,8 +54,6 @@ export const Resume: React.FC<ResumeProps> = () => {
             </div>
             {showProfileAbout && <p className="mb-4 p-8 text-2xl text-blue-100">{EricResumeDescription}</p>}
           </section>
-          <div className="h-40" />
-          <div className="h-40" />
         </>
       )}
 
